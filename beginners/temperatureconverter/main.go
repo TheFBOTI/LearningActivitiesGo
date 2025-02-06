e33b1f3d305c027d60f1f5f2253cc6b48ca5159b
TheFBOTI <martyn.b.se@gmail.com>
2025-02-06 16:58:19 +0000

    feat: Started to add scanner input for users to declare what type of conversion they would like

--- beginners/temperatureconverter/main.go
+++ beginners/temperatureconverter/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Converting temperatures")
+	var temperatureType string
+	fmt.Println("Please enter the type of temperature you wish to convert to")
+	fmt.Scanln(&temperatureType)
 
+	if temperatureType != "c" && temperatureType != "f" && !strings.EqualFold("celsius", temperatureType) && !strings.EqualFold("fahrenheit", temperatureType) {
+		fmt.Println("Unknown temperature type provided, exiting")
+		os.Exit(1)
+	} else if temperatureType == "c" || temperatureType == "f" {
+		switch temperatureType {
+		case "c":
+			fmt.Println("Celsius has been selected")
+		case "f":
+			fmt.Println("Fahrenheit has been selected")
+		}
+	} else {
+		fmt.Println(temperatureType + " has been selected")
+	}
 	fmt.Println("Fahrenheit is worked out by taking Celsius and multipling by 1.8, then adding 32.")
 
 	celsius := 10
